02075eca65aa64e13d3d29fa3b7fefa9f77b1a27
Exanite <exanite78012@gmail.com>
2022-09-30 00:27:04 -0400

    Add room size cost array

--- Project/Assets/Prototype/Exanite/ProcGen/LevelGenerator.cs
+++ Project/Assets/Prototype/Exanite/ProcGen/LevelGenerator.cs
@@ -1,7 +1,5 @@
-using System;
 using Exanite.Drawing;
 using UnityEngine;
-using Random = UnityEngine.Random;
 
 namespace Prototype.Exanite.ProcGen
 {
@@ -13,11 +11,21 @@ namespace Prototype.Exanite.ProcGen
         [Header("Settings")]
         public Vector2Int Size = Vector2Int.one * 10;
 
+        [Space]
+
+        // Walk
         public float WalkPerlinScale = 1;
         public Vector2 WalkPerlinOffset = Vector2.one;
         public float WalkPerlinMultiplier = 1;
-
         public float WalkRandomMultiplier = 1;
+
+        [Space]
+
+        // RoomSize
+        public float RoomSizePerlinScale = 1;
+        public Vector2 RoomSizePerlinOffset = Vector2.one;
+        public float RoomSizePerlinMultiplier = 1;
+        public float RoomSizeRandomMultiplier = 1;
 
         [Header("Seed")]
         public bool UseSeed;
@@ -27,11 +35,17 @@ namespace Prototype.Exanite.ProcGen
         public Color WalkCostMin = Color.green;
         public Color WalkCostMax = Color.red;
 
+        [Space]
+        public Color RoomSizeCostMin = Color.yellow;
+        public Color RoomSizeCostMax = Color.magenta;
+
         [Header("Generation")]
         public bool GenerateContinuously;
         public float GenerationDelay = 0.25f;
 
         private float[,] walkCosts;
+        private float[,] roomSizeCosts;
+
         private float generationTimer;
 
         private void Start()
@@ -66,6 +80,7 @@ namespace Prototype.Exanite.ProcGen
                 Random.InitState(Seed.GetHashCode());
             }
 
+            // Walk costs
             var walkPerlinOffsetFromSeed = new Vector2(Random.Range(0f, 10000f), Random.Range(0f, 10000f));
 
             walkCosts = new float[Size.x, Size.y];
@@ -91,6 +106,33 @@ namespace Prototype.Exanite.ProcGen
                     walkCosts[x, y] += Random.Range(0f, WalkRandomMultiplier);
                 }
             }
+
+            // Room size costs
+            var roomSizePerlinOffsetFromSeed = new Vector2(Random.Range(0f, 10000f), Random.Range(0f, 10000f));
+
+            roomSizeCosts = new float[Size.x, Size.y];
+
+            for (var y = 0; y < roomSizeCosts.GetLength(1); y++)
+            {
+                for (var x = 0; x < roomSizeCosts.GetLength(0); x++)
+                {
+                    var perlin = Mathf.PerlinNoise(
+                        x * RoomSizePerlinScale + RoomSizePerlinOffset.x + roomSizePerlinOffsetFromSeed.x,
+                        y * RoomSizePerlinScale + RoomSizePerlinOffset.y + roomSizePerlinOffsetFromSeed.y);
+
+                    perlin *= RoomSizePerlinMultiplier;
+
+                    roomSizeCosts[x, y] += perlin;
+                }
+            }
+
+            for (var y = 0; y < roomSizeCosts.GetLength(1); y++)
+            {
+                for (var x = 0; x < roomSizeCosts.GetLength(0); x++)
+                {
+                    roomSizeCosts[x, y] += Random.Range(0f, RoomSizeRandomMultiplier);
+                }
+            }
         }
 
         private void OnRenderObject()
@@ -112,6 +154,24 @@ namespace Prototype.Exanite.ProcGen
                     {
                         handle.Color = Color.Lerp(WalkCostMin, WalkCostMax, walkCosts[x, y] / maxWalkCost);
                         handle.DrawCube(Vector3.right * x + Vector3.forward * y, Quaternion.identity, new Vector3(0.9f, 0.1f, 0.9f));
+                    }
+                }
+
+                var maxRoomSizeCost = 0.001f;
+                for (var y = 0; y < roomSizeCosts.GetLength(1); y++)
+                {
+                    for (var x = 0; x < roomSizeCosts.GetLength(0); x++)
+                    {
+                        maxRoomSizeCost = Mathf.Max(maxRoomSizeCost, roomSizeCosts[x, y]);
+                    }
+                }
+
+                for (var y = 0; y < roomSizeCosts.GetLength(1); y++)
+                {
+                    for (var x = 0; x < roomSizeCosts.GetLength(0); x++)
+                    {
+                        handle.Color = Color.Lerp(RoomSizeCostMin, RoomSizeCostMax, roomSizeCosts[x, y] / maxRoomSizeCost);
+                        handle.DrawCube(Vector3.right * x + Vector3.forward * y + Vector3.down, Quaternion.identity, new Vector3(0.9f, 0.1f, 0.9f));
                     }
                 }
             }
