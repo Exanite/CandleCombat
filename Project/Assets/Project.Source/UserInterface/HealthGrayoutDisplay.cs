c3a07f248ed9ccc1d1cc4e43b522fd4b14915123
Exanite <exanite78012@gmail.com>
2022-10-01 19:10:47 -0400

    Add screen grayout when low hp

---
+++ Project/Assets/Project.Source/UserInterface/HealthGrayoutDisplay.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.Rendering;
+
+namespace Project.Source.UserInterface
+{
+    public class HealthGrayoutDisplay : MonoBehaviour
+    {
+        public Volume Volume;
+        public AnimationCurve GrayoutCurve;
+
+        private void Update()
+        {
+            var healthRatio = Mathf.Clamp01(GameContext.Instance.CurrentHealth / GameContext.Instance.MaxHealth);
+            var weight = Mathf.Clamp01(GrayoutCurve.Evaluate(1 - healthRatio));
+
+            Volume.weight = weight;
+        }
+    }
+}
