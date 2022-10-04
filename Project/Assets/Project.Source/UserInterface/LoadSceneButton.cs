0e44cb4f8bae5c7d74b87acd39f8f705abc468c9
Exanite <exanite78012@gmail.com>
2022-10-04 15:43:31 -0400

    Move MenuSceneScript to Project.Source and rename

--- Project/Assets/Project.Source/UserInterface/LoadSceneButton.cs
+++ Project/Assets/Project.Source/UserInterface/LoadSceneButton.cs
@@ -1,12 +1,10 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
-public class MainMenuScript : MonoBehaviour
+public class LoadSceneButton : MonoBehaviour
 {
-    [SerializeField] private Animator animator = null;
+    [SerializeField]
+    private Animator animator;
 
     public string MainSceneName = "Main";
 
@@ -24,6 +22,4 @@ public class MainMenuScript : MonoBehaviour
     {
         SceneManager.LoadScene(MainSceneName);
     }
-
-    
 }
