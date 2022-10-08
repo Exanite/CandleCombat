952c41839990852f370747bdf7b42c25a0581b04
Exanite <exanite78012@gmail.com>
2022-10-07 22:28:57 -0400

    Update serialized data

--- Project/Assets/Project.Source/Gameplay/Player/HumanPlayerController.cs
+++ Project/Assets/Project.Source/Gameplay/Player/HumanPlayerController.cs
@@ -2,30 +2,20 @@ using Project.Source.Input;
 using UniDi;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using UnityEngine.Serialization;
 
 namespace Project.Source.Gameplay.Player
 {
     public class HumanPlayerController : PlayerController
     {
         [Header("Dependencies")]
-        [FormerlySerializedAs("shootReference")]
         [SerializeField] private InputActionReference shootInput;
-        [FormerlySerializedAs("movementReference")]
         [SerializeField] private InputActionReference movementInput;
-        [FormerlySerializedAs("pointerReference")]
         [SerializeField] private InputActionReference pointerInput;
-        [FormerlySerializedAs("dodgeReference")]
         [SerializeField] private InputActionReference dodgeInput;
-        [FormerlySerializedAs("reloadReference")]
         [SerializeField] private InputActionReference reloadInput;
-        [FormerlySerializedAs("quitReference")]
         [SerializeField] private InputActionReference quitInput;
 
         private Plane Plane => new Plane(Vector3.up, Vector3.up);
-
-        [Inject]
-        private GameContext gameContext;
 
         [Inject]
         private Camera mainCamera;
