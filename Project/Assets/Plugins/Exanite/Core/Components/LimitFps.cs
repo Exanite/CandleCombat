8a2deb425e507215abd76a59127ab6aa6acfd9b6
Exanite <exanite78012@gmail.com>
2022-10-11 14:56:28 -0400

    Replace LimitBatchModeFrameRate with LimitFps

---
+++ Project/Assets/Plugins/Exanite/Core/Components/LimitFps.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace Exanite.Core.Components
+{
+    /// <summary>
+    ///     Sets the target frame rate to the specified TargetFps on Start.
+    ///     Can be optionally set to run only when
+    ///     <see cref="Application.isBatchMode"/> is true is used.
+    /// </summary>
+    public class LimitFps : MonoBehaviour
+    {
+        public bool IsBatchModeOnly;
+        public int TargetFps = 30;
+
+        private void Start()
+        {
+            if (Application.isBatchMode || !IsBatchModeOnly)
+            {
+                QualitySettings.vSyncCount = 0;
+                Application.targetFrameRate = TargetFps;
+            }
+        }
+    }
+}
