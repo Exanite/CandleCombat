eb0156500614b3c692172ba45fc1c1f344929790
Kyle Dampier <30601856+kyledampier@users.noreply.github.com>
2022-10-25 14:39:23 -0400

    changed coupling algorithm to combine two best

--- MachineLearning/src/neural_network.py
+++ MachineLearning/src/neural_network.py
@@ -1,24 +1,109 @@
 import tensorflow as tf
+import numpy as np
+import random
+
+def is_number(val):
+    if type(val) == type(0.0) \
+        or type(val) == type(0) \
+        or type(val) == np.float32 \
+        or type(val) == np.float64 \
+        or type(val) == np.int32 \
+        or type(val) == np.int64:
+        return True
+    return False
+
+def print_arr_shape(arr, name="test", i=0):
+    while not is_number(arr):
+        tabs = "\t" * i
+        print(f"{tabs}len({name}) = {len(arr)}")
+        for j in range(len(arr)):
+            print_arr_shape(arr[j], name=f"{name}[{j}]", i=(i+1))
+        break
+
+def get_weights_mutated(first, second, mutation_rate):
+
+    if is_number(first) and is_number(second):
+        # randomly switch biases between the two
+        pct = random.random()
+        if pct >= 0.5:
+            return first
+        else:
+            return second
+
+    out = []
+    # print("\t\t\tlen(first) = %d, len(second) = %d" % (len(first), len(second)))
+    for i in range(len(first)):
+        # get the random average of the weights from the two parents
+        pct = random.random() # 0 - 1 uniform distribution
+        weight_first = (first[i] * pct)
+        weight_second = (second[i] * (1 - pct))
+        weight = weight_first + weight_second
+        # TODO: mutate the weight by some normal distribution
+        # mutation = np.random.normal(weight, mutation_rate, weight.shape)
+        out.append(weight)
+    return out
 
 class NeuralNetwork:
-    def __init__(self, input_size, output_size, hidden_layers, learning_rate):
+    def __init__(self, input_size, output_size, hidden_layers, learning_rate, init_std=5.0):
         self.input_size = input_size
         self.output_size = output_size
         self.hidden_layers = hidden_layers
         self.learning_rate = learning_rate
+        self.init_std = init_std
 
         self._build_model()
 
     def _build_model(self):
+        initializer = tf.keras.initializers.RandomNormal(stddev=self.init_std)
+
         self.model = tf.keras.Sequential()
-        self.model.add(tf.keras.layers.Dense(self.input_size, input_dim=self.input_size, activation='relu'))
+        # input layer
+        self.model.add(tf.keras.layers.Dense(self.input_size, input_dim=self.input_size, activation='relu', 
+                        kernel_initializer=initializer,
+                        bias_initializer=initializer))
+        # hidden layers
         for layer in self.hidden_layers:
-            self.model.add(tf.keras.layers.Dense(layer, activation='relu'))
-        self.model.add(tf.keras.layers.Dense(self.output_size, activation='linear'))
+            self.model.add(tf.keras.layers.Dense(layer, activation='relu',
+                            kernel_initializer=initializer,
+                            bias_initializer=initializer))
+
+        # output layer
+        self.model.add(tf.keras.layers.Dense(self.output_size, activation='tanh',
+            kernel_initializer=initializer,
+            bias_initializer=initializer))
+
         self.model.compile(loss='mse', optimizer=tf.keras.optimizers.Adam(lr=self.learning_rate))
 
     def predict(self, inputs):
         return self.model.predict(inputs)
 
-    def train(self, inputs, targets):
-        self.model.fit(inputs, targets, epochs=1, verbose=0)
+    def new_generation(self, second_best=None, mutation_rate=None):
+        if mutation_rate == None:
+            mutation_rate = self.learning_rate
+        # get the generation of the current model
+        weights_best = self.model.get_weights()
+        weights_second_best = second_best.model.get_weights()
+        output_weights = []
+
+        # print_arr_shape(weights_best, name='weights_best')
+
+        # for each layer
+        for k in range(len(weights_best)):
+            node = weights_best[k]
+            out = []
+            # for each node
+            for i in range(len(node)):
+                new_weights = get_weights_mutated(weights_best[k][i], weights_second_best[k][i], mutation_rate)
+                out.append(new_weights)
+            output_weights.append(out)
+        # print_arr_shape(output_weights, name='output_weights')
+        output = NeuralNetwork(self.input_size, self.output_size, self.hidden_layers, self.learning_rate)
+        output.model.set_weights([np.array(a) for a in output_weights])
+        output.model.compile(loss='mse', optimizer=tf.keras.optimizers.Adam(lr=self.learning_rate))
+        return output
+
+    def save(self, path):
+        self.model.save(path)
+
+    def load(self, path):
+        self.model = tf.keras.models.load_model(path)
--- MachineLearning/train.py
+++ MachineLearning/train.py
@@ -4,13 +4,15 @@ import random
 import json
 import time
 import multiprocessing
+import asyncio
 import pandas as pd
 import numpy as np
-# import src.neural_network as nn
+import src.neural_network as nn
+import pygad
 
-pipe_name = "Pipe1"
-num_instances = 1
-exe_path = os.path.join("Builds", "UI", "Project.exe")
+pipe_name = "Pipe4"
+num_instances = 50
+exe_path = os.path.join("Builds", "Server", "Project.exe")
 
 
 def get_distances_from_player(player, enemy):
@@ -43,10 +45,9 @@ def get_row_from_obj(obj):
     input_obj["enemy_offset_y"] = 0
     if len(obj["Enemies"]) > 0:
         nearest_enemy = {}
-        nearest_enemy_distance = np.finfo(np.float64).max
+        nearest_enemy_distance = 100000000
         for i in range(len(obj["Enemies"])):
             enemy = obj["Enemies"][i]
-            print("enemy", enemy)
             if enemy['CanSeeFromPlayer'] or True:
                 input_obj["has_enemy"] = 1
                 enemy_dist = get_distances_from_player(player, enemy)
@@ -56,7 +57,7 @@ def get_row_from_obj(obj):
                     nearest_enemy_distance = enemy_dist
 
         if nearest_enemy != {}:
-            input_obj["enemy_distance"] = nearest_enemy_distance
+            input_obj["enemy_distance"] = -1
             input_obj["enemy_offset_x"] = nearest_enemy["OffsetFromPlayer"]["x"]
             input_obj["enemy_offset_y"] = nearest_enemy["OffsetFromPlayer"]["y"]
 
@@ -66,9 +67,8 @@ def get_row_from_obj(obj):
 def start_pipe():
     print("starting game")
     os.system(
-        f"{exe_path} --instance-count {num_instances} --pipe-name {pipe_name}")
+        f"{exe_path} --instance-count {num_instances} --pipe-name {pipe_name} --respawn-player False")
     print("finished")
-
 
 def main():
 
@@ -91,49 +91,77 @@ def main():
     }
 
     count = 0
-    times_run = 1000
+    times_run = 100000
+    max_results = -1
+    count_last_max = 0
     f = open(pipe_path, "r+")
+    models = {}
+    results = {}
+    history = []
 
-    while True:
+    while count < times_run:
         line = f.readline()
-        # print(f"--- INPUT {count} ---")
-        # print(line)
         player_arr = json.loads(line)
         if len(player_arr) == 0:
             break
 
-        inputs = []
-        rewards = []
-
         for i in range(len(player_arr)):
-            player = player_arr[i]
-            input_obj, reward = get_row_from_obj(player)
-            inputs.append(input_obj)
-            rewards.append(reward)
+            if not player_arr[i]["Id"] in models:
+                player = player_arr[i]
+                if len(models) < num_instances:
+                    print(f"Initializing model for player {player['Id']}")
+                    models[player['Id']] = nn.NeuralNetwork(24, 9, [64, 32], 1.0, init_std=5.0)
+                    models[player['Id']]._build_model()
+                    results[player['Id']] = 0.0
+                else:
+                    max_id = max(results, key=results.get)
+                    max_results = results[max_id]
+                    del results[max_id]
+                    second_max_id = max(results, key=results.get)
+                    second_max_results = results[second_max_id]
+                    results[max_id] = max_results
 
-        print(pd.DataFrame(inputs))
-        print(f"TIME SURVIVED: {rewards}")
+                    # save best model
+                    file_name = f"model_{max_id[0:7]}.h5"
+                    models[max_id].save(os.path.join("models", file_name))
+
+                    second_best = models[second_max_id]
+                    print(f"Generating new model for player {player['Id']} from players: \n\tbest={max_id} ({max_results})\n\tsecond_best={second_max_id} ({second_max_results}) \nSaved Best: {file_name}\n{'-'*16}")
+                    models[player['Id']] = models[max_id].new_generation(second_best=second_best)
+                    results[player['Id']] = 0.0
+                    history.append(results[max_id])
 
         output = []
-        sum_time = 0
+        predictions = []
 
         for player in player_arr:
-            sum_time += player["Player"]["TimeAlive"]
+            in_arr, time_alive = get_row_from_obj(player)
+            # print(f"Predicting for player {player['Id']}, time alive: {time_alive}")
+            prediction = models[player['Id']].predict([list(in_arr.values())])
+            predictions.append(prediction[0])
+
+        for prediction in predictions:
             tmp = base_player.copy()
 
-            tmp["MovementDirection"]["x"] = (random.random() - 0.5) * 2
-            tmp["MovementDirection"]["y"] = (random.random() - 0.5) * 2
-            tmp["TargetDirection"]["x"] = (random.random() - 0.5) * 2
-            tmp["TargetDirection"]["y"] = (random.random() - 0.5) * 2
+            tmp["MovementDirection"]["x"] = float(prediction[0])
+            tmp["MovementDirection"]["y"] = float(prediction[1])
+            tmp["TargetDirection"]["x"] = float(prediction[2])
+            tmp["TargetDirection"]["y"] = float(prediction[3])
+            tmp["IsBurningShotPressed"] = float(prediction[4]) > 0.5
+            tmp["IsSoulTransferPressed"] = float(prediction[5]) > 0.5
+            tmp["IsDodgePressed"] = float(prediction[6]) > 0.5
+            tmp["IsShootPressed"] = float(prediction[7]) > 0.5
+            tmp["IsReloadPressed"] = float(prediction[8]) > 0.5
 
             output.append(tmp)
+            results[player['Id']] = time_alive
 
-        print(f"AVG TIME - {sum_time/len(player_arr)}")
-
-        if count == times_run:
-            break
-
-        time.sleep(0.2)
+        timed_runs = list(results.values())
+        tmp_max_results = max(timed_runs)
+        if tmp_max_results > max_results:
+            max_results = tmp_max_results
+            print(f"New max results: {max_results} took {count - count_last_max} runs")
+            count_last_max = count
 
         out = json.dumps(output).replace(" ", "") + "\n"
         # print(f"--- OUTPUT {count} ---")
@@ -148,6 +176,6 @@ def main():
 if __name__ == "__main__":
     process = multiprocessing.Process(target=start_pipe)
     process.start()
-    time.sleep(7)
+    time.sleep(10)
     main()
     process.join()
